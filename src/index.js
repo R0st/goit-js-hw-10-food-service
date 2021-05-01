2e99aa700c420f3406eaf0b89456e25e5a32d526
R0st <dotarost@mail.ru>
2021-05-02 00:10:24 +0300

    add template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+import menuTmp from './templates/menu.hbs';
+import menuTmpOne from './templates/menuOne.hbs';
+import menuTmpData from './menu.json';
+
 import './styles.css';
+
+const iconTmp = document.querySelector('.js-menu');
+const markup = menuTmp(menuTmpData);
+iconTmp.insertAdjacentHTML('beforeend', markup);
+console.log(menuTmp(menuTmpData));
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+// console.log(localStorage);
+// localStorage.setItem('my-data', JSON.stringify({ name: 'mango', age: 2 }));
+
+
+// const savedData = localStorage.getItem('my-data');
+// console.log('savedData', savedData);
+
+// const parsedData = JSON.parse(savedData);
+// console.log('parsedData', parsedData);
