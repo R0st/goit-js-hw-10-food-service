d402a798a536f2a5758932ed725bf5c13d940e53
R0st <dotarost@mail.ru>
2021-05-04 22:35:22 +0300

    add theme-no change

--- src/index.js
+++ src/index.js
@@ -9,10 +9,48 @@ const markup = menuTmp(menuTmpData);
 iconTmp.insertAdjacentHTML('beforeend', markup);
 console.log(menuTmp(menuTmpData));
 
+// CHANGE THEME
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
+const bodyTheme = document.querySelector('body');
+const checkboxEl = document.querySelector('.theme-switch__toggle');
+
+
+bodyTheme.classList = localStorage.getItem('theme');
+// if (localStorage.getItem('theme') === Theme.DARK) {
+//   localStorage.getItem('theme');
+// }
+
+if (localStorage.getItem('theme') === Theme.DARK) {
+    checkboxEl.checked = true;
+}
+checkboxEl.addEventListener('change', onInputChangeTheme);
+
+function onInputChangeTheme() {
+    if (localStorage.getItem('theme') === Theme.LIGHT) {
+        localStorage.removeItem('theme', Theme.LIGHT);
+        localStorage.setItem('theme', Theme.DARK);
+        bodyTheme.classList.remove(Theme.LIGHT);
+        bodyTheme.classList.add(Theme.DARK);
+        
+    } if (localStorage.getItem('theme') === Theme.DARK) {
+            localStorage.removeItem('theme', Theme.DARK);
+            localStorage.setItem('theme', Theme.LIGHT);
+            bodyTheme.classList.remove(Theme.DARK);    
+            bodyTheme.classList.add(Theme.LIGHT);   
+        }
+};
+
+
+// bodyTheme.classList = localStorage
+//     ? localStorage.getItem('theme')
+//     : localStorage.getItem('theme')===Theme.LIGHT;
+// const savedLocal = localStorage.getItem('theme');
+// bodyTheme.classList = savedLocal;
+// }
 
 // console.log(localStorage);
 // localStorage.setItem('my-data', JSON.stringify({ name: 'mango', age: 2 }));
